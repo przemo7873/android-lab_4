aa723f17bc9a13f67a08e00ab37b73a3de40b88f
przemo7873 <62542229+przemo7873@users.noreply.github.com>
2020-06-15 23:10:25 +0200

    listview

--- app/src/main/java/com/example/android_lab/DodajWpis.java
+++ app/src/main/java/com/example/android_lab/DodajWpis.java
@@ -2,7 +2,10 @@ package com.example.android_lab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class DodajWpis extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class DodajWpis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
+    }
+
+    public void wyslij(View view)
+    {
+        EditText kontrolka = (EditText)findViewById(R.id.editText);
+        String pole = kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis",pole);
+        setResult(RESULT_OK,intencja);
+        finish();;
     }
 }
